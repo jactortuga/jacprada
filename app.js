56c4f1ac55742af3ed245326ff9e814604c873a6
jactortuga <jac@tortugalabs.io>
2017-09-16 16:53:48 +0100

    first commit, importing code

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var siteLinks = document.getElementsByTagName('a');
+
+  // for (var i = 0; i < siteLinks.length; i++) {
+  //   siteLinks[i].addEventListener('mouseenter', function(){
+  //     this.classList.remove('hover');
+  //   }, false);
+  // }
+
+});
